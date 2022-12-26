d292632835194dd946167ddbedf7d76c7f31605e
Sam Lu <549429518@qq.com>
2022-12-26 17:46:57 +0800

    Implement MetadataDecoder

--- src/Compilers/Shared/CodeAnalysis/Symbols/Metadata/PE/MetadataDecoder.cs
+++ src/Compilers/Shared/CodeAnalysis/Symbols/Metadata/PE/MetadataDecoder.cs
@@ -2,6 +2,7 @@
 // The Qtyi licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Diagnostics;
 using Roslyn.Utilities;
 
 #if LANG_LUA
@@ -23,20 +24,40 @@ internal partial class MetadataDecoder : Microsoft.CodeAnalysis.MetadataDecoder<
 
     internal PEModuleSymbol ModuleSymbol => base.moduleSymbol;
 
+    /// <summary>
+    /// 使用类型上下文创建<see cref="MetadataDecoder"/>的新实例。
+    /// </summary>
+    /// <param name="moduleSymbol">元数据所处的PE模块符号。</param>
+    /// <param name="context">元数据所处的类型上下文。</param>
     public MetadataDecoder(
         PEModuleSymbol moduleSymbol,
         PENamedTypeSymbol context)
         : this(moduleSymbol, context, null) { }
 
+    /// <summary>
+    /// 使用方法上下文创建<see cref="MetadataDecoder"/>的新实例。
+    /// </summary>
+    /// <param name="moduleSymbol">元数据所处的PE模块符号。</param>
+    /// <param name="context">元数据所处的方法上下文。</param>
     public MetadataDecoder(
         PEModuleSymbol moduleSymbol,
         PEMethodSymbol context)
         : this(moduleSymbol, (PENamedTypeSymbol?)context.ContainingType, context) { }
 
+    /// <summary>
+    /// 创建<see cref="MetadataDecoder"/>的新实例。
+    /// </summary>
+    /// <param name="moduleSymbol">元数据所处的PE模块符号。</param>
     public MetadataDecoder(
         PEModuleSymbol moduleSymbol)
         : this(moduleSymbol, null, null) { }
 
+    /// <summary>
+    /// 通用构造函数。
+    /// </summary>
+    /// <param name="moduleSymbol">元数据所处的PE模块符号。</param>
+    /// <param name="typeContext">元数据所处的类型上下文。</param>
+    /// <param name="methodContext">元数据所处的方法上下文。</param>
     private MetadataDecoder(
         PEModuleSymbol moduleSymbol,
         PENamedTypeSymbol? typeContext,
@@ -54,6 +75,29 @@ internal partial class MetadataDecoder : Microsoft.CodeAnalysis.MetadataDecoder<
     protected override TypeSymbol GetGenericMethodTypeParamSymbol(int position)
     {
         if (this._methodContext is null)
-            return new 
+            return new UnsupportedMetadataTypeSymbol(); // 类型参数不与方法关联。
+
+        Debug.Assert(position >= 0);
+
+        var typeParameters = this._methodContext.TypeParameters;
+        if (typeParameters.Length <= position)
+            return new UnsupportedMetadataTypeSymbol(); // 类型参数的位置过大。
+
+        return typeParameters[position];
+    }
+
+    protected override TypeSymbol GetGenericTypeParamSymbol(int position)
+    {
+        var type = this._typeContext;
+        while (type is not null && (type.MetadataArity - type.Arity) > position)
+            type = type.ContainingSymbol as PENamedTypeSymbol;
+
+        if (type is null || type.MetadataArity <= position)
+            return new UnsupportedMetadataTypeSymbol(); // 类型参数的位置过大。
+
+        position -= type.MetadataArity - type.Arity;
+        Debug.Assert(position >= 0 && position < type.Arity);
+
+        return type.TypeParameters[position];
     }
 }
--- src/Compilers/Shared/CodeAnalysis/Symbols/Metadata/PE/PENamedTypeSymbol.cs
+++ src/Compilers/Shared/CodeAnalysis/Symbols/Metadata/PE/PENamedTypeSymbol.cs
@@ -2,7 +2,11 @@
 // The Qtyi licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Reflection;
+using System.Reflection.Metadata;
+using System.Reflection.Metadata.Ecma335;
 using Microsoft.CodeAnalysis;
+using Qtyi.CodeAnalysis.Lua.Symbols.PublicModel;
 
 #if LANG_LUA
 namespace Qtyi.CodeAnalysis.Lua.Symbols.Metadata.PE;
@@ -10,6 +14,45 @@ namespace Qtyi.CodeAnalysis.Lua.Symbols.Metadata.PE;
 namespace Qtyi.CodeAnalysis.MoonScript.Symbols.Metadata.PE;
 #endif
 
-internal sealed partial class PENamedTypeSymbol : NamedTypeSymbol
+internal abstract partial class PENamedTypeSymbol : NamedTypeSymbol
 {
+    private readonly ModuleSymbol _container;
+    private readonly TypeDefinitionHandle _handle;
+    private readonly string _name;
+    private readonly TypeAttributes _flags;
+    private readonly SpecialType _corTypeId;
+
+    /// <summary>
+    /// 获取逻辑上包含了此符号的PE文件的.NET模块符号。
+    /// </summary>
+    /// <value>
+    /// 逻辑上包含了此符号的PE文件的.NET模块符号。
+    /// 若此符号不属于任何.NET模块，或在多个.NET模块间共享，则返回<see langword="null"/>。
+    /// </value>
+    internal PEModuleSymbol ContainingPEModule
+    {
+        get
+        {
+            var s = this._container;
+
+            while (s.Kind != SymbolKind.Namespace)
+                s = s.ContainingSymbol;
+
+            return ((PENamespaceSymbol)s).ContainingPEModule;
+        }
+    }
+
+    /// <inheritdoc/>
+    internal override NetModuleSymbol? ContainingNetModule => this.ContainingPEModule;
+
+    public override SpecialType SpecialType => this._corTypeId;
+
+    public abstract override int Arity { get; }
+
+    internal abstract int MetadataArity { get; }
+
+    internal TypeDefinitionHandle Handle => this._handle;
+
+    public override int MetadataToken => MetadataTokens.GetToken(_handle);
+
 }
